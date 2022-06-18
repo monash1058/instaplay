a78ba325262bdfc5f5e98a61d2bfcae2b7e4016c
monash1058 <monash.karthi@gmail.com>
2022-06-18 16:43:37 +0530

    changes

--- src/app/home/movie-list/movie-list.component.ts
+++ src/app/home/movie-list/movie-list.component.ts
@@ -9,10 +9,13 @@ import { HomeService } from '../home.service';
 })
 export class MovieListComponent implements OnInit {
   moviesData: any = [];
-  searchValue: any = '';
   p: any = 1;
   count: any = 10;
   show: boolean = false;
+  apiResponse: any;
+  searchQuery = '';
+  timer:any = null;
+  noResult = false;
   constructor(private homeService: HomeService) {
     this.homeService.searchBoolean.next(true);
   }
@@ -20,11 +23,10 @@ export class MovieListComponent implements OnInit {
   ngOnInit(): void {
     this.getMoviesData();
     this.homeService.searchData.subscribe((res: any) => {
-      if (res.value === '') {
-        this.getMoviesData();
-      } else if (res.value) {
-        this.searchValue = res.value;
-        this.getSearch();
+      console.log(res.value);
+      if (res.value) {
+        this.getSearch(res.value);
+      } else {
       }
     });
   }
@@ -34,19 +36,37 @@ export class MovieListComponent implements OnInit {
       .getMethod(path)
       .pipe(take(1))
       .subscribe((res: any) => {
-        console.log(res);
+        this.apiResponse = res.results;
         this.moviesData = res.results;
-        console.log(this.moviesData);
       });
   }
-  getSearch() {
-    this.moviesData = [];
-    this.homeService
-      .searchPostMethod(this.searchValue)
-      .pipe(take(1))
-      .subscribe((res: any) => {
-        this.moviesData = res.results;
+  // getSearch(res:any) {
+  //   this.moviesData = [];
+  //   this.homeService
+  //     .searchPostMethod(res)
+  //     .pipe(take(1))
+  //     .subscribe((res: any) => {
+  //       this.moviesData = res.results;
+  //     });
+  // }
+  getSearch(searchStr: string) {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      searchStr = searchStr.trim();
+      if (searchStr === '') {
+        this.moviesData = this.apiResponse;
+        return;
+      }
+      this.homeService.searchPostMethod(searchStr).pipe(take(1)).subscribe((data:any) => {
+        this.noResult = false;
+        if (data.total_results === 0) {
+          this.moviesData = [];
+          this.noResult = true;
+          return;
+        }
+        this.moviesData = data.results;
       });
+    }, 250);
   }
   sortLow() {
     this.moviesData.sort((a: any, b: any) => {
